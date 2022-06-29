a085712571a7e6c1b0bedb65cdf494f91aefee3f
Your Name <phunglam11d@gamil.com>
2022-06-29 10:31:01 +0700

    nhanh new

--- src/main/java/com/example/tryhart11/service/category/ICategoryService.java
+++
@@ -1,7 +0,0 @@
-package com.example.tryhart11.service.category;
-
-import com.example.tryhart11.model.Category;
-import com.example.tryhart11.service.IGeneralService;
-
-public interface ICategoryService extends IGeneralService<Category> {
-}
